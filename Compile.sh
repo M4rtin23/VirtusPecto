a00fb9150b1c13e520149344e7c8636a139a7c5a
M4rtin23 <martunico23@hotmail.com>
2019-10-13 23:38:07 -0300

    Removed files

--- Compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd '/home/martin/Documentos/Projectos/C#/VirtusPecto/'
-csc 'Behaviors.cs' 'Buttons/PlayButton.cs' 'Buttons/SettingsButton.cs' 'GameMaker.cs' 'Buttons/WindowBox.cs' 'Buttons/AspectBox.cs' 'Card.cs' 'CardContent.cs' 'Creature.cs' 'CreatureDatabase.cs' 'Buttons/DifficultyBox.cs' 'Enemy.cs' 'FireBall.cs' 'Buttons/Fullscreen.cs' 'Game1.cs' 'GameMouse.cs' 'Level.cs' 'Lobby.cs' 'Mouse.cs' 'PauseMenu.cs' 'Player.cs' 'Program.cs' 'SettingsMenu.cs' 'ToolBar.cs' 'Buttons/WinBorder.cs' -r:'Game/MonoGame.Framework.dll' 
-if [ -f 'Program.exe' ]
-then
-mv 'Program.exe' './Game'
-cd './Game'
-mono Program.exe
-fi
