5517952ed4b6826bce24a3e16a9468df874660a2
M4rtin23 <martunico23@hotmail.com>
2020-10-30 20:06:45 -0300

    Erased TickBox Classes

--- src/GUI/Buttons/TickBox.cs
+++ src/GUI/Buttons/TickBox.cs
@@ -8,16 +8,22 @@ namespace VirtusPecto.Desktop{
 	public class TickBox{
 		protected string name;
 		protected Vector2 position;
-		protected virtual bool state{get; set;}
+		protected bool state = false;
 		bool checker;
 		
-		public void Update(float x, float y){
+		public TickBox(string name){
+			this.name = name;
+		}
+
+		public void Update(float x, float y, ref bool state){
+			this.state = state;
 			position = new Vector2(x, y);
 			if(new Rectangle((int)position.X,(int) position.Y, 32, 32).Contains(Mouse1.Position)){
-				GameMouse.Click(() => {state = !state;;}, ref checker);
+				GameMouse.Click(() => {this.state = !this.state;}, ref checker);
 			}else{
 				checker = false;
 			}
+			state = this.state;
 		}
 		
 		public void Draw(SpriteBatch batch) {
--- src/GUI/SettingsMenu.cs
+++ src/GUI/SettingsMenu.cs
@@ -8,33 +8,36 @@ using static GameBuilder.GameBase;
 namespace VirtusPecto.Desktop{
 	public class SettingsMenu{
 		public WindowBox ResolutionBox;
-		public Fullscreen SetFullscreen;
-		public Nearest ShowNearest;
-		public Direction ShowDirection;
+		public TickBox SetFullscreen;
+		public TickBox ShowNearest;
+		public TickBox ShowDirection;
 		public AspectBox SetAspectRatio;
-		public Joystick SetJoystick;
-		public CardDescription ShowDescription;
+		public TickBox SetJoystick;
+		public TickBox ShowDescription;
 		private Button Back = new Button("Go Back", () => {GoToPrevious(); Settings = null;});
 		public Vector2 AspectRatio{get => SetAspectRatio.Options[SetAspectRatio.Option];}
+
 		public SettingsMenu(){
-			ShowDescription = new CardDescription();
-			ShowNearest = new Nearest();
-			ShowDirection = new Direction();
-			SetJoystick = new Joystick();
+			ShowDescription = new TickBox("Show Description");
+			ShowNearest = new TickBox("Mark Nearest Enemy");
+			ShowDirection = new TickBox("Show Mouse Direction");
+			SetJoystick = new TickBox("Joystick");
 			SetAspectRatio = new AspectBox();
-			SetFullscreen = new Fullscreen();
+			SetFullscreen = new TickBox("Fullscreen");
 			ResolutionBox = new WindowBox();
 		}
 
 		public void Update(){
-			ShowDirection.Update(Width * 3/4 - 64, 2*Height / 3);
-			ShowNearest.Update(Width * 2/4 - 64, 2*Height / 3);
+			bool temp = IsFullscreen;
+			ShowDirection.Update(Width * 3/4 - 64, 2*Height / 3, ref Game1.ShowDirection);
+			ShowNearest.Update(Width * 2/4 - 64, 2*Height / 3, ref Game1.ShowNearest);
 			SetAspectRatio.Update(Width / 4 - 64,  Height / 3);
-			ShowDescription.Update(Width * 2/4 - 64,  Height / 2);
-			SetFullscreen.Update(Width * 3/4 - 64,  Height / 3);
-			SetJoystick.Update(Width * 3/4 - 64,  Height / 2);
+			ShowDescription.Update(Width * 2/4 - 64,  Height / 2, ref IsDescriptionOn);
+			SetFullscreen.Update(Width * 3/4 - 64,  Height / 3, ref temp);
+			SetJoystick.Update(Width * 3/4 - 64,  Height / 2,  ref IsJoystick);
 			ResolutionBox.Update(Width * 2/4 - 64, Height / 3);
 			Back.Update(0, 0);
+			IsFullscreen = temp;
 		}
 		public void Draw(SpriteBatch batch){
 			Back.Draw(batch);			
